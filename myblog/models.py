62374abe6680944b0d565a43f416899debebb899
JHAN0907 <anmj1222@naver.com>
2018-07-23 13:34:19 +0900

    first

---
+++ myblog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) #게시자 변수
+    title = models.CharField(max_length =200) #제목
+    text = models.TextField() #본문
+    created_data = models.DateTimeField(default = timezone.now) #작성 시간
+    published_data = models.DateTimeField(blank = True, null= True)
+
+    def publish(self):
+        self.published_data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
