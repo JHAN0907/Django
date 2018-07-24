74dee7f0620cadf54dfcdf02e2f2c0fb7a135238
JHAN0907 <anmj1222@naver.com>
2018-07-24 19:36:34 +0900

    Django fix

--- myblog/models.py
+++ myblog/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE) #게시자 변수
     title = models.CharField(max_length =200) #제목
     text = models.TextField() #본문
-    created_data = models.DateTimeField(default = timezone.now) #작성 시간
-    published_data = models.DateTimeField(blank = True, null= True)
+    created_date = models.DateTimeField(default = timezone.now) #작성 시간
+    published_date = models.DateTimeField(blank = True, null= True)
 
     def publish(self):
         self.published_data = timezone.now()
